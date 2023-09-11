6e25c6797b18fa880fc4ecda521554ac4095baa0
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-09-11 10:20:39 -0500

    Refactor even more two number sum exercise

--- two-number-sum.js
+++ two-number-sum.js
@@ -2,21 +2,25 @@
 
 function twoNumberSum(array, targetSum) {
   // Write your code here.
-  //   let auxSum;
-  //   for (var i = 0; i < array.length - 1; i++) {
-  //     for (var j = i + 1; j < array.length; j++) {
-  //       auxSum = array[i] + array[j];
+  //Best solution that came up to my mind
+  /*
+     let auxSum;
+    for (var i = 0; i < array.length - 1; i++) {
+      for (var j = i + 1; j < array.length; j++) {
+        auxSum = array[i] + array[j];
 
-  //       if (auxSum === targetSum) {
-  //         return [array[i], array[j]];
-  //       }
-  //     }
-  //   }
-  //   return [];
+        if (auxSum === targetSum) {
+          return [array[i], array[j]];
+        }
+      }
+    }
+    return [];
+  */
 
+  // My solution
   // Different approach implemented by me having into consideration optimal space and time complexity O(n) time where n is the lentgh of the input array
-
-  var hashTable = array.reduce(function (map, obj) {
+  /*
+    var hashTable = array.reduce(function (map, obj) {
     if (targetSum - obj != obj) {
       let substraction = targetSum - obj;
       map[obj] = array.includes(substraction);
@@ -27,6 +31,17 @@ function twoNumberSum(array, targetSum) {
   return Object.keys(hashTable)
     .filter((key) => hashTable[key] === true)
     .map((value) => parseInt(value, 10));
+    */
+
+  // Even Best Solution Found
+  let seen = new Set();
+  let complement;
+  for (let i in array) {
+    complement = targetSum - array[i];
+    if (seen.has(array[i])) return [complement, array[i]];
+    seen.add(complement);
+  }
+  return [];
 }
 
 // Do not edit the line below.
